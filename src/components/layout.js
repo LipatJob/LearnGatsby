f61851f361d9bf765a53649081aa20eb3049ee36
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-03-13 12:06:23 +0800

    added contact to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,6 +35,9 @@ const Layout = ({ pageTitle, children }) => {
           <li className={navLinkItem}>
             <Link to="/blog" className={navLinkText}>Blog</Link>
           </li>
+          <li className={navLinkItem}>
+            <Link to="/layout" className={navLinkText}>Blog</Link>
+          </li>
         </ul>
       </nav>
       <main>
